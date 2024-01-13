8c13fe10859d3f20820145e6c1d689da09aa2822
Dilshan <mdd06307@gmail.com>
2024-01-14 00:06:35 +0530

    third commit

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("hello java with jenkins");
         System.out.println("This is Another Loop");
         System.out.println(new Date());
+        Student student =new Student("Dilshad Alam");
+        System.out.println(student.getName());
     }
 }
