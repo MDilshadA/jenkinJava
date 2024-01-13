3c80f803af8dc4807825271130901de98e24059f
Dilshan <mdd06307@gmail.com>
2024-01-14 00:00:49 +0530

    second commit

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello java with jenkins");
         System.out.println("This is Another Loop");
+        System.out.println(new Date());
     }
 }
